59ae589295990d2efc5541b5fe60eab72f1984b7
Javier López <javierlopezc16@gmail.com>
2023-04-17 14:29:23 -0400

    first commit

---
+++ heli_product_extension/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Heli Forklift Extensión de Productos",
+
+    'summary': """
+        """,
+
+    'description': """
+        Heli Forklift Extensión de Productos
+    """,
+
+    'author': "Heli Forklift, Javier López",
+    'website': "heliforklift.cl",
+    'category': 'Retail',
+    'version': '16.0.1',
+    'depends': [
+        'product',
+    ],
+
+    'data': [
+        
+    ],
+    'license': 'LGPL-3',
+}
