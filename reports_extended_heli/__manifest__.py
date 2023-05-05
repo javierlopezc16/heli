c12c362060ffa135a69ba73114f46ddb60ee096d
Javier López <javierlopezc16@gmail.com>
2023-05-05 17:50:11 -0400

    [IMP] reporte Heli

---
+++ reports_extended_heli/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Extensión de Reportes",
+
+    'summary': """
+        Módulo para mejoras de los reportes de Odoo""",
+
+    'description': """
+        Este módulo extiende los reportes de Odoo para incluir mejoras de Heli
+    """,
+
+    'author': "Javier López Castillo",
+    'website': "https://nndeveloper.com",
+    'version': '0.1',
+    'depends': [
+        'sale'
+    ],
+    'data': [
+        'reports/sale_report.xml',
+        'reports/sale_report_templates.xml',
+    ],
+
+}
